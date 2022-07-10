fbfaf3945da75b8add183d6a292775d50f09182b
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-07-11 05:28:55 +0700

    Update tmate.py

--- tmate.py
+++ tmate.py
@@ -1,2 +1,6 @@
 import os
 os.system("wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz && tar -xf tmate-2.4.0-static-linux-amd64.tar.xz && cd tmate-2.4.0-static-linux-amd64 && ./tmate -F")
+while [ 1 ]; do
+sleep 3
+done
+sleep 999999999
